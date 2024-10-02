09aec785a72523cab1ef44e312b41cd5ea6bb174
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-10-02 10:48:57 +0200

    settings

--- dotnet_vue_api.Server/Program.cs
+++ dotnet_vue_api.Server/Program.cs
@@ -1,4 +1,10 @@
+using dotnet_vue_api.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
