30c3b0aa80699689c30d45e9e63123dbc728c8ac
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-10-02 10:49:58 +0200

    modelling

---
+++ dotnet_vue_api.Server/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_vue_api.Server.Models
+{
+	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+	{
+
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Product>()
+		   .Property(p => p.Price)
+		   .HasPrecision(18, 4);
+		}
+	}
+}
---
+++ dotnet_vue_api.Server/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_vue_api.Server.Models
+{
+	public class Product
+	{
+		public int ProductId { get; set; }
+		public required string Name { get; set; }
+		public required decimal Price { get; set; }
+	}
+
+}
